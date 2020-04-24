151dabb6549a69bf5c64feb7e499f196b2739dff
NikitaLu <abcdlumin@126.com>
2020-04-24 17:27:25 +0800

    for object detection record

--- Cameo.py
+++ Cameo.py
@@ -6,15 +6,19 @@ class Cameo(object):
     def __init__(self):
         self._windowManager=WindowManager("Cameo",self.onKeypress)
         self._captureManager=CaptureManager(cv2.VideoCapture(0),self._windowManager,True)
-        self._curveFilter=filters.BGRPortraCurveFilter()
+        self._curveFilter=filters.EmbossFilter()
 
     def run(self):
         self._windowManager.createWindow()
         while self._windowManager.isWindowCreated:
             self._captureManager.enterFrame()
             frame=self._captureManager.frame
+            filters.strokeEdges(frame,frame)
+            self.captureFilter.apply(frame,frame)
+
             self._captureManager.exitFrame()
             self._windowManager.processEvents()
+
     def onKeypress(self,keycode):
         if keycode==32:
             self._captureManager.writeImage("screenshot.png")
@@ -25,5 +29,6 @@ class Cameo(object):
                 self._captureManager.stopWritingVideo()
         elif keycode==27:
             self._windowManager.destoryWindow()
+
 if __name__=="__main__":
     Cameo().run()
